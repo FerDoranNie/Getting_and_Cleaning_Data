57cd5da52576f8a75fe97d808f084d17fd780c35
JhossePaul <mcromanceva@hotmail.com>
2015-07-24 12:43:33 -0500

    Agregando paqueterías packrat

--- download.R
+++ download.R
@@ -1,6 +1,4 @@
 # Download and unzip data
-tmp = tempfile()
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-              destfile = tmp, method = "curl")
-file.copy(tmp, "courseraproj.zip")
+              destfile = "courseraproj.zip", method = "curl")
 unzip("courseraproj.zip")
--- script.R
+++ script.R
@@ -1,9 +1,6 @@
-tmp = tempfile()
-download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-              destfile = tmp, method = "curl")
-file.copy(tmp, "courseraproj.zip")
-unzip("courseraproj.zip")
-
-
+library(data.table)
 X = read.table("UCI HAR Dataset/test/X_test.txt", quote = " ")
 readLines("UCI HAR Dataset/test/y_test.txt")
+
+
+t
